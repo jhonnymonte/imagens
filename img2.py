54ef3e4896124a0aff9d3177dd88cd289db91624
jhonnymonte <mrmontenegro@gmail.com>
2022-01-13 22:37:26 -0300

    arme la funcion de crear carpeta.

--- img2.py
+++ img2.py
@@ -1,10 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import time
+# URL Hardcodeada para hacerlo mas rapido
+url = "https://www.fortnitecountdown.com/tienda/actual/"
 
-#URL Hardcodeada para hacerlo mas rapido
-url ="https://www.fortnitecountdown.com/tienda/actual/"
 
-#send get request
+def create_folder(path):
+    ''' la funcion crea las carpetas de forma automatica y notifica la creacion de los mismos
+    '''
+    try:
+        file_path = os.getcwd() + path
+        print(file_path)
+        # Determinar si el directorio ya existe
+        if not os.path.exists(file_path):
+            # El directorio no existe, crea la operación
+            # Utilice el método os.makedirs () para crear directorios de niveles múltiples
+            os.makedirs(file_path)
+            print("Directorio creado con éxito:" + file_path)
+        else:
+            print("¡El directorio ya existe!")
+    except BaseException as msg:
+        print("Error al crear el nuevo directorio:" + msg)
+
+
+def fetch(url, type):
+    ''' la funcion invoca a la funcion create folder y crea carpetacorrespondiente con la fecha automatica
+        la funcion toma 2 argumentos la url y un string para el nombre de la carpeta por ejemplo "fornite"
+    '''
+
+
+    with requests.Session() as s:
+        folder = f"./{type}"
+        create_folder(folder)
+        # send get request
+        
+        
 response = requests.get(url)
 
 html_page = BeautifulSoup(response.text, 'html.parser')
@@ -12,16 +43,16 @@ html_page = BeautifulSoup(response.text, 'html.parser')
 images = html_page.find_all("img")
 
 for index, image in enumerate(images):
-    image_url= image.get("src")      #img src value
-    
-    #tomar la extension de la imagen
-    image_extension= image_url.split(".")[-1]       
+    image_url = image.get("src")  # img src value
 
-    #get image data
+    # tomar la extension de la imagen
+    image_extension = image_url.split(".")[-1]
+
+    # get image data
     image_bytes = requests.get(image_url).content
-    
+
     if image_bytes:
-        #write the image data
+        # write the image data
         with open(f"Image {index+1}.{image_extension}", "wb") as file:
             file.write(image_bytes)
             print(f"Downloading image {index+1}.{image_extension}")
